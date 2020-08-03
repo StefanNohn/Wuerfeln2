36032afc327518e8cb7a4fa9c02140d9f546c3b8
StefanNohn <snohn@outlook.com>
2020-08-03 19:53:32 +0200

    Fehler Dice wird nicht richtig verglichen

--- Form1.cs
+++ Form1.cs
@@ -25,57 +25,8 @@ namespace Würfeln
 
             for (int i = 0; i <= 9; i++)
             {
-                ListeSpieler[i] = new Spieler(PunkteAnzeigen, SpielerAktivInaktiv);
-                ListeTextBoxSpieler[i] = new TextBox();
-                ListeTextBoxSpieler[i].Visible = false;
-
-                ListeLabelSpieler[i] = new Label();
-                ListeLabelSpieler[i].AutoSize = true;
-                ListeLabelSpieler[i].Size = new System.Drawing.Size(100, 13);
-                ListeLabelSpieler[i].Location = new System.Drawing.Point(P1, Reihe + 24 * i);
-                ListeLabelSpieler[i].TabIndex = 0;
-                ListeLabelSpieler[i].Text = ListeSpieler[i].Name;
-                ListeLabelSpieler[i].MouseClick += new System.Windows.Forms.MouseEventHandler(this.LabelSpieler_MouseClick);
-
-                ListeComboBoxSpieler[i] = new ComboBox();
-                ListeComboBoxSpieler[i].FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-                ListeComboBoxSpieler[i].FormattingEnabled = true;
-                ListeComboBoxSpieler[i].Items.AddRange(new object[] {
-                "Mensch",
-                "KI - ängstlich",
-                "KI - normal",
-                "KI - risiko"});
-                ListeComboBoxSpieler[i].Visible = true;
-                ListeComboBoxSpieler[i].Size = new System.Drawing.Size(92, 21);
-                ListeComboBoxSpieler[i].Location = new System.Drawing.Point(P1 + 92, Reihe - 3 + 24 * i);
-                ListeComboBoxSpieler[i].TabIndex = 9;
-                ListeComboBoxSpieler[i].SelectedValueChanged += new System.EventHandler(this.ComboBoxSpieler_SelectedValueChanged);
-
-                ListeLabelRundeAnzeige[i] = new Label();
-                ListeLabelRundeAnzeige[i].Location = new System.Drawing.Point(P1 + 200, Reihe + 24 * i);
-                ListeLabelRundeAnzeige[i].AutoSize = true;
-                ListeLabelRundeAnzeige[i].TabIndex = 0;
-                ListeLabelRundeAnzeige[i].Text = "Runde: ";
-
-                ListeLabelRunde[i] = new Label();
-                ListeLabelRunde[i].AutoSize = true;
-                ListeLabelRunde[i].Location = new System.Drawing.Point(P1 + 260, Reihe + 24 * i);
-                ListeLabelRunde[i].TabIndex = 0;
-                ListeLabelRunde[i].Text = $"{Runde}";
-                               
-                ListeLabelPunkte[i] = new Label();
-                ListeLabelPunkte[i].AutoSize = true;                
-                ListeLabelPunkte[i].Location = new System.Drawing.Point(P1 + 280, Reihe + 24 * i);
-                ListeLabelPunkte[i].TabIndex = 0;
-                ListeLabelPunkte[i].Text = "Punkte: ";
-
-                ListeLabelPunkteAnzeige[i] = new Label();
-                ListeLabelPunkteAnzeige[i].AutoSize = true;
-                ListeLabelPunkteAnzeige[i].Location = new System.Drawing.Point(P1 + 330, Reihe + 24 * i);
-                ListeLabelPunkteAnzeige[i].TabIndex = 0;
-                ListeLabelPunkteAnzeige[i].TextAlign = System.Drawing.ContentAlignment.TopRight;
-                ListeLabelPunkteAnzeige[i].Text = "123456";
-
+                Spieler.ListeSpieler[i] = new Spieler(PunkteAnzeigen,/* SpielerAktivInaktiv,*/ i);
+               
                 if (i <= 5)
                 {
                     ListeDice[i]= new Dice();
@@ -88,19 +39,12 @@ namespace Würfeln
         //        ListeSpieler,
         //    }
         
-        private Spieler[] ListeSpieler = new Spieler[10];
-        private Label[] ListeLabelSpieler = new Label[10];
-        private Label[] ListeLabelRunde = new Label[10];        
-        private ComboBox[] ListeComboBoxSpieler = new ComboBox[10];
-        private Label[] ListeLabelRundeAnzeige = new Label[10];
-        private Label[] ListeLabelPunkte = new Label[10];
-        private Label[] ListeLabelPunkteAnzeige = new Label[10];
-        private TextBox[] ListeTextBoxSpieler = new TextBox[10];
+       
       
         private Dice[] ListeDice = new Dice[6];
         
-        private int _Runde=0;
-        public int Runde
+        private static int _Runde=0;
+        public static int Runde
         {
             get { return _Runde; }
             set { _Runde = value; }
@@ -112,94 +56,32 @@ namespace Würfeln
             get { return _AngelegteSpieler; }
             set { _AngelegteSpieler = value; }
         }
-        const int Reihe = 12;
-        const int P1 = 9;
+       
         public void SpielerHinzufügen()
         {
             if (AngelegterSpieler < 10)
             {
-                this.Controls.Add(ListeLabelSpieler[AngelegterSpieler]); 
-                this.Controls.Add(ListeComboBoxSpieler[AngelegterSpieler]);              
-                this.Controls.Add(ListeLabelRunde[AngelegterSpieler]);
-                this.Controls.Add(ListeLabelRundeAnzeige[AngelegterSpieler]);
-                this.Controls.Add(ListeLabelPunkte[AngelegterSpieler]);                
-                this.Controls.Add(ListeLabelPunkteAnzeige[AngelegterSpieler]);                              
-
+                this.Controls.Add(Spieler.ListeLabelSpieler[AngelegterSpieler]); 
+                this.Controls.Add(Spieler.ListeComboBoxSpieler[AngelegterSpieler]);              
+                this.Controls.Add(Spieler.ListeLabelRunde[AngelegterSpieler]);
+                this.Controls.Add(Spieler.ListeLabelRundeAnzeige[AngelegterSpieler]);
+                this.Controls.Add(Spieler.ListeLabelPunkte[AngelegterSpieler]);                
+                this.Controls.Add(Spieler.ListeLabelPunkteAnzeige[AngelegterSpieler]);
+                this.Controls.Add(Spieler.ListeTextBoxSpieler[AngelegterSpieler]);
                 AngelegterSpieler++;
             }            
         }
          
         private void PunkteAnzeigen(int SpielerNr, int Punkte)
         {
-            int a = Convert.ToInt32(ListeLabelPunkte[SpielerNr].Text);
-            ListeLabelPunkte[SpielerNr].Text = Convert.ToString(Punkte + a);
+            int a = Convert.ToInt32(Spieler.ListeLabelPunkte[SpielerNr].Text);
+            Spieler.ListeLabelPunkte[SpielerNr].Text = Convert.ToString(Punkte + a);
         }
 
-        private int WerIstSender(object sender)
-        {           
-            int a = 10;
-            for (int i=0;i<10;i++)
-            {
-                if ((sender == ListeLabelSpieler[i]) || (sender == ListeTextBoxSpieler[i]) || (sender== ListeComboBoxSpieler[i]) || (sender == ListeSpieler[i]))
-                { a= i; }
-            };
-            return a;
-        }
-        private void LabelSpieler_MouseClick(object sender, MouseEventArgs e)
-        {
-            int Sender= WerIstSender(sender);
-            //if (ComboBoxSpieler1.SelectedIndex == 0)    Erlaubt nur die Benennung eines menschlichen Spielers, nimmt aber Moeglichkeiten und evtl. Spassfaktor
-            //{
-            //ListeTextBoxSpieler[Sender].AutoSize = true;
-            ListeTextBoxSpieler[Sender].Location = new Point(ListeLabelSpieler[Sender].Location.X, ListeLabelSpieler[Sender].Location.Y-2);
-            ListeTextBoxSpieler[Sender].Text = ListeLabelSpieler[Sender].Text;
-            ListeTextBoxSpieler[Sender].KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextBoxSpieler_KeyDown);
-            ListeTextBoxSpieler[Sender].Leave += new System.EventHandler(this.TextBoxSpieler_Leave);
-            ListeTextBoxSpieler[Sender].Visible = true;
-            ListeLabelSpieler[Sender].Visible = false;
-            ListeTextBoxSpieler[Sender].AcceptsReturn = false;
-            this.Controls.Add(ListeTextBoxSpieler[Sender]);
-            //}
-        }
-        private void TextBoxSpieler_KeyDown(object sender, KeyEventArgs e)
-        {
-            int Sender = WerIstSender(sender);
-               
-            if ((e.KeyCode == Keys.Enter)||(e.KeyCode == Keys.Escape))
-            {
-                if (ListeTextBoxSpieler[Sender].Text.Length >= 1) 
-                { 
-                    ListeLabelSpieler[Sender].Text = ListeTextBoxSpieler[Sender].Text; 
-                }
-                else ListeLabelSpieler[Sender].Text = "Ohne Namen";                
-                e.Handled = true;
-                e.SuppressKeyPress = true;
-                ListeTextBoxSpieler[Sender].Visible = false;
-                ListeLabelSpieler[Sender].Visible = true;
-            } 
-        }
-        private void TextBoxSpieler_Leave(object sender, EventArgs e)
-        {
-            int Sender = WerIstSender(sender);
+       
+       
 
-
-            if (ListeTextBoxSpieler[Sender].Text.Length >= 1)
-            {
-                ListeLabelSpieler[Sender].Text = ListeTextBoxSpieler[Sender].Text;
-            }
-            else ListeLabelSpieler[Sender].Text = "Ohne Namen";
-
-            ListeTextBoxSpieler[Sender].Visible = false;
-            ListeLabelSpieler[Sender].Visible = true;
-        }           
-        
-        private void ComboBoxSpieler_SelectedValueChanged(object sender, EventArgs e)
-        {
-            int Sender = WerIstSender(sender);
-            ListeLabelSpieler[Sender].Text = ListeComboBoxSpieler[Sender].Items[ListeComboBoxSpieler[Sender].SelectedIndex].ToString();           
-        }
-               
-        private int WelcherDice(object sender)
+        public int WelcherDice(object sender)
         {
             int a=11;
             for (int i=0;i<6;i++)
@@ -211,7 +93,7 @@ namespace Würfeln
             }
             return a;
         }
-        private void Clicked(int Sender)
+        public void Clicked(int Sender)
         {
             if (!ListeDice[Sender].Used)
             {
@@ -219,7 +101,7 @@ namespace Würfeln
             }
             else ListeDice[Sender].Used = false;
         }
-        private void Dice_Click(object sender, EventArgs e)
+        public void Dice_Click(object sender, EventArgs e)
         {
             int Sender=WelcherDice(sender);
             Clicked(Sender);            
@@ -273,11 +155,15 @@ namespace Würfeln
             SpielerHinzufügen();
         }
 
-        private void SpielerAktivInaktiv(object SpielerName, bool AktivInaktiv)
+        public static void SpielerAktivInaktiv(object SpielerName, bool AktivInaktiv)
         {
-            int Sender = WerIstSender(SpielerName);
-            if (AktivInaktiv) ListeLabelSpieler[Sender].ForeColor = Color.Red;
-            else ListeLabelSpieler[Sender].ForeColor = Color.Black;
+            int Sender = Spieler.WerIstSender(SpielerName);
+            if (Spieler.ListeLabelSpieler[Sender]!=null)
+            {
+                if (AktivInaktiv) Spieler.ListeLabelSpieler[Sender].ForeColor = Color.Red;
+                else Spieler.ListeLabelSpieler[Sender].ForeColor = Color.Black;
+            }
+            
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -305,7 +191,7 @@ namespace Würfeln
         {
             for (int i = 0; i < 10; i++)
             {
-                ListeSpieler[i].Aktiv = false;
+                Spieler.ListeSpieler[i].Aktiv = false;
             }
         }
         private void ButtonSpielSteuerung_Click(object sender, EventArgs e)
@@ -315,7 +201,7 @@ namespace Würfeln
                 SpielerAnfangInaktiv();
                 Runde = 1;
                 SpielerAktuell = 0;
-                ListeSpieler[SpielerAktuell].Aktiv = true;              
+                Spieler.ListeSpieler[SpielerAktuell].Aktiv = true;              
                 ButtonSpielerHinzufügen.Visible = false;
                 ButtonSpielSteuerung.Text = "Nächster Spieler";
                 LabelNachrichten.Visible = false;
--- Spieler.cs
+++ Spieler.cs
@@ -4,31 +4,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing;
 
 namespace Würfeln
 {
     public class Spieler
-    {        
+    {
+        public static Spieler[] ListeSpieler = new Spieler[10];
+        public static Label[] ListeLabelSpieler = new Label[10];
+        public static Label[] ListeLabelRunde = new Label[10];
+        public static ComboBox[] ListeComboBoxSpieler = new ComboBox[10];
+        public static Label[] ListeLabelRundeAnzeige = new Label[10];
+        public static Label[] ListeLabelPunkte = new Label[10];
+        public static Label[] ListeLabelPunkteAnzeige = new Label[10];
+        public static TextBox[] ListeTextBoxSpieler = new TextBox[10];
+
+        const int Reihe = 12;
+        const int P1 = 9;
+
         private List<int> ListePunkte = new List<int>();
-               
+
         public delegate void PunkteAusgabe(int SpielerNr, int Punkte);
         private PunkteAusgabe _PunkteAufLabel = null;
         public void PunkteAufLabel(PunkteAusgabe a)
         {
             _PunkteAufLabel += a;
         }
-        public delegate void SpielerStatus(object Spieler, bool AktivInaktiv);
-        private SpielerStatus _AktivInaktiv;
-        public void AktivInaktiv(SpielerStatus a)
-        {
-            _AktivInaktiv += a;
-        }
+        //public delegate void SpielerStatus(object Spieler, bool AktivInaktiv);
+        //private SpielerStatus _AktivInaktiv;
+        //public void AktivInaktiv(SpielerStatus a)
+        //{
+        //    _AktivInaktiv += a;
+        //}
 
         private string _Name;
         public string Name
         {
             get { return _Name; }
             set { _Name = value; }
+        }
+
+        private int _Nr;
+        public int Nr
+        {
+            get { return _Nr; }
+            set { _Nr = value; }
         }
 
         private bool _Aktiv;
@@ -40,25 +60,142 @@ namespace Würfeln
                 _Aktiv = value;
                 if (_Aktiv)
                 {
-                    _AktivInaktiv(this, true);
+                    Form1.SpielerAktivInaktiv(this, true);
                 }
                 if (!_Aktiv)
                 {
-                    _AktivInaktiv(this, false);
+                    Form1.SpielerAktivInaktiv(this, false);
                 }
             }
         }
 
         public Spieler()
-        {}
+        { }
 
-        public Spieler(PunkteAusgabe b, SpielerStatus c /*, Form1 d*/)
+        public Spieler(PunkteAusgabe b,/* SpielerStatus c,*/ int i)
         {
             Name = "Neuer Spieler";
             PunkteAufLabel(b);
             ListePunkte.Add(0);
-            AktivInaktiv(c);
-            //Aktiv = false;           
+            Nr = i;
+            //AktivInaktiv(c);
+            if (i == 0) Aktiv = true;
+            else Aktiv = false;
+            //erstellen der Anzeigen von Spielername, Spielertypauswahl, Rundenanzeige und Punkteanzeige
+            ListeLabelSpieler[i] = new Label();
+            ListeLabelSpieler[i].AutoSize = true;
+            ListeLabelSpieler[i].Size = new System.Drawing.Size(100, 13);
+            ListeLabelSpieler[i].Location = new System.Drawing.Point(P1, Reihe + 24 * i);
+            ListeLabelSpieler[i].TabIndex = 0;
+            ListeLabelSpieler[i].Text = "Spieler";
+            ListeLabelSpieler[i].MouseClick += new System.Windows.Forms.MouseEventHandler (LabelSpieler_MouseClick);
+
+            ListeTextBoxSpieler[i] = new TextBox();
+            ListeTextBoxSpieler[i].Visible = false;
+                      
+            ListeComboBoxSpieler[i] = new ComboBox();
+            ListeComboBoxSpieler[i].FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            ListeComboBoxSpieler[i].FormattingEnabled = true;
+            ListeComboBoxSpieler[i].Items.AddRange(new object[] {
+                "Mensch",
+                "KI - ängstlich",
+                "KI - normal",
+                "KI - risiko"});
+            ListeComboBoxSpieler[i].Visible = true;
+            ListeComboBoxSpieler[i].Size = new System.Drawing.Size(92, 21);
+            ListeComboBoxSpieler[i].Location = new System.Drawing.Point(P1 + 92, Reihe - 3 + 24 * i);
+            ListeComboBoxSpieler[i].TabIndex = 9;
+            ListeComboBoxSpieler[i].SelectedValueChanged += new System.EventHandler(this.ComboBoxSpieler_SelectedValueChanged);
+
+            ListeLabelRundeAnzeige[i] = new Label();
+            ListeLabelRundeAnzeige[i].Location = new System.Drawing.Point(P1 + 200, Reihe + 24 * i);
+            ListeLabelRundeAnzeige[i].AutoSize = true;
+            ListeLabelRundeAnzeige[i].TabIndex = 0;
+            ListeLabelRundeAnzeige[i].Text = "Runde: ";
+
+            ListeLabelRunde[i] = new Label();
+            ListeLabelRunde[i].AutoSize = true;
+            ListeLabelRunde[i].Location = new System.Drawing.Point(P1 + 260, Reihe + 24 * i);
+            ListeLabelRunde[i].TabIndex = 0;
+            ListeLabelRunde[i].Text = $"{Form1.Runde}";
+
+            ListeLabelPunkte[i] = new Label();
+            ListeLabelPunkte[i].AutoSize = true;
+            ListeLabelPunkte[i].Location = new System.Drawing.Point(P1 + 280, Reihe + 24 * i);
+            ListeLabelPunkte[i].TabIndex = 0;
+            ListeLabelPunkte[i].Text = "Punkte: ";
+
+            ListeLabelPunkteAnzeige[i] = new Label();
+            ListeLabelPunkteAnzeige[i].AutoSize = true;
+            ListeLabelPunkteAnzeige[i].Location = new System.Drawing.Point(P1 + 330, Reihe + 24 * i);
+            ListeLabelPunkteAnzeige[i].TabIndex = 0;
+            ListeLabelPunkteAnzeige[i].TextAlign = System.Drawing.ContentAlignment.TopRight;
+            ListeLabelPunkteAnzeige[i].Text = "123456";
+        }
+
+        public static void LabelSpieler_MouseClick(object sender, MouseEventArgs e)
+        {                       
+            int Sender = WerIstSender(sender);
+            //if (ComboBoxSpieler1.SelectedIndex == 0)    Erlaubt nur die Benennung eines menschlichen Spielers, nimmt aber Moeglichkeiten und evtl. Spassfaktor
+            //{
+            //ListeTextBoxSpieler[Sender].AutoSize = true;
+            Spieler.ListeTextBoxSpieler[Sender].Location = new Point(Spieler.ListeLabelSpieler[Sender].Location.X, Spieler.ListeLabelSpieler[Sender].Location.Y - 2);
+            Spieler.ListeTextBoxSpieler[Sender].Text = Spieler.ListeLabelSpieler[Sender].Text;
+            Spieler.ListeTextBoxSpieler[Sender].KeyDown += new System.Windows.Forms.KeyEventHandler(TextBoxSpieler_KeyDown);
+            Spieler.ListeTextBoxSpieler[Sender].Leave += new System.EventHandler(Spieler.TextBoxSpieler_Leave);
+            Spieler.ListeTextBoxSpieler[Sender].Visible = true;
+            Spieler.ListeLabelSpieler[Sender].Visible = false;
+            Spieler.ListeTextBoxSpieler[Sender].AcceptsReturn = false;
+            // Form1.Controls.Add(Spieler.ListeTextBoxSpieler[Sender]);
+            //}
+        }
+        public static void TextBoxSpieler_KeyDown(object sender, KeyEventArgs e)
+        {
+            int Sender = WerIstSender(sender);
+
+            if ((e.KeyCode == Keys.Enter) || (e.KeyCode == Keys.Escape))
+            {
+                if (ListeTextBoxSpieler[Sender].Text.Length >= 1)
+                {
+                    ListeLabelSpieler[Sender].Text = ListeTextBoxSpieler[Sender].Text;
+                }
+                else ListeLabelSpieler[Sender].Text = "Ohne Namen";
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+                ListeTextBoxSpieler[Sender].Visible = false;
+                ListeLabelSpieler[Sender].Visible = true;
+            }
+        }
+        public static void TextBoxSpieler_Leave(object sender, EventArgs e)
+        {
+            int Sender = WerIstSender(sender);
+
+
+            if (ListeTextBoxSpieler[Sender].Text.Length >= 1)
+            {
+                ListeLabelSpieler[Sender].Text = ListeTextBoxSpieler[Sender].Text;
+            }
+            else ListeLabelSpieler[Sender].Text = "Ohne Namen";
+
+            ListeTextBoxSpieler[Sender].Visible = false;
+            ListeLabelSpieler[Sender].Visible = true;
+        }
+
+        public void ComboBoxSpieler_SelectedValueChanged(object sender, EventArgs e)
+        {
+            int Sender = WerIstSender(sender);
+            ListeLabelSpieler[Sender].Text = ListeComboBoxSpieler[Sender].Items[ListeComboBoxSpieler[Sender].SelectedIndex].ToString();
+        }
+
+        public static int WerIstSender(object sender)
+        {
+            int a = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                if ((sender == Spieler.ListeLabelSpieler[i]) || (sender == Spieler.ListeTextBoxSpieler[i]) || (sender == Spieler.ListeComboBoxSpieler[i]) || (sender == Spieler.ListeSpieler[i]))
+                { a = i; }
+            };
+            return a;
         }
     }
 }
